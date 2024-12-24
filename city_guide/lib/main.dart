86c9b4ab8e29d1c34c0d12967624e6507e962a65
Daniyal <daniyal2472@gmail.com>
2024-12-24 17:47:28 +0500

    firebase connected

--- city_guide/lib/main.dart
+++ city_guide/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:city_guide/AuthScreens/login.dart';
 import 'package:city_guide/Utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: primary),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: LoginScreen(),
     );
   }
 }
